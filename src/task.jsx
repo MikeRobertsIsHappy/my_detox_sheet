2bbcb064f71b7d66f07db4f31f92ed40eede7e0e
ListeningJackal <MikeRobertsIsHappy@gmail.com>
2021-07-24 20:19:08 -0700

    Roughly done
    Roughly done

---
+++ src/task.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Draggable } from 'react-beautiful-dnd';
+
+const Container = styled.div`
+  border: 1px solid lightgrey;
+  border-radius: 0px;
+  padding: 0px;
+  width : 200px
+  margin-bottom: 0px;
+  background-color: ${props =>
+    props.isDragDisabled
+      ? 'lightgrey'
+      : props.isDragging
+        ? 'lightgreen'
+        : 'white'};
+`;
+
+export default class Task extends React.Component {
+  render() {
+    const isDragDisabled = this.props.task.id === 'task-0';
+    return (
+      <Draggable
+        draggableId={this.props.task.id}
+        index={this.props.index}
+        isDragDisabled={isDragDisabled}
+      >
+        {(provided, snapshot) => (
+          <Container
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            innerRef={provided.innerRef}
+            isDragging={snapshot.isDragging}
+            isDragDisabled={isDragDisabled}
+          >
+            {this.props.task.content}
+          </Container>
+        )}
+      </Draggable>
+    );
+  }
+}
